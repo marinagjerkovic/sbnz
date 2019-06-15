00394b7fff81bba9d70bf4db6acf6ce190993149
ikijaem <ilija.malinovic@yahoo.com>
2019-06-15 16:52:48 +0200

    popravljanje pravila

--- drools-spring-app/src/main/java/sbnz/integracija/siem_center/SiemCenterService.java
+++ drools-spring-app/src/main/java/sbnz/integracija/siem_center/SiemCenterService.java
@@ -47,22 +47,38 @@ public class SiemCenterService {
 		/*
 		//simulations
 		//login error on same machine
-		Log logError1 = new Log (LogType.Login, new Machine ("192.168.20.60", false, OperatingSystem.Windows), new User("username1", Risk.Low,LocalDateTime.parse("2019-04-08T11:11:00", DateTimeFormatter.ISO_LOCAL_DATE_TIME)),LocalDateTime.parse("2019-04-23T11:11:00", DateTimeFormatter.ISO_LOCAL_DATE_TIME), "failed login", LogStatus.Error);
-		Log logError2 = new Log (LogType.Login, new Machine ("192.168.20.60", false, OperatingSystem.Windows), new User("username2", Risk.Low,LocalDateTime.parse("2019-04-08T11:12:00", DateTimeFormatter.ISO_LOCAL_DATE_TIME)),LocalDateTime.parse("2019-04-23T11:12:00", DateTimeFormatter.ISO_LOCAL_DATE_TIME), "failed login", LogStatus.Error);
+		Machine machine = new Machine("192.168.20.60", false, OperatingSystem.Windows);
+		Log logError1 = new Log (LogType.Login, machine, new User("username1", Risk.Low,LocalDateTime.parse("2019-04-08T11:11:00", DateTimeFormatter.ISO_LOCAL_DATE_TIME)),LocalDateTime.parse("2019-04-23T11:11:00", DateTimeFormatter.ISO_LOCAL_DATE_TIME), "failed login", LogStatus.Error);
+		Log logError2 = new Log (LogType.Login, machine, new User("username2", Risk.Low,LocalDateTime.parse("2019-04-08T11:12:00", DateTimeFormatter.ISO_LOCAL_DATE_TIME)),LocalDateTime.parse("2019-04-23T11:12:00", DateTimeFormatter.ISO_LOCAL_DATE_TIME), "failed login", LogStatus.Error);
 		
 		//login error with same username
 		User us = new User("username1", Risk.Low,LocalDateTime.parse("2019-04-08T11:11:00", DateTimeFormatter.ISO_LOCAL_DATE_TIME));
 		Log logError1 = new Log (LogType.Login, new Machine ("192.168.20.60", false, OperatingSystem.Windows), us, LocalDateTime.parse("2019-04-23T11:11:00", DateTimeFormatter.ISO_LOCAL_DATE_TIME), "failed login", LogStatus.Error);
 		Log logError2 = new Log (LogType.Login, new Machine ("192.168.20.70", false, OperatingSystem.Windows), us, LocalDateTime.parse("2019-04-23T11:12:00", DateTimeFormatter.ISO_LOCAL_DATE_TIME), "failed login", LogStatus.Error);		
-		*/
+		
 		
 		//90 days inactive
 		User us = new User("username1", Risk.Low, LocalDateTime.parse("2018-04-08T11:11:00", DateTimeFormatter.ISO_LOCAL_DATE_TIME));
-		Log logError1 = new Log (LogType.Login, new Machine ("192.168.20.60", false, OperatingSystem.Windows), us, LocalDateTime.parse("2019-04-23T11:11:00", DateTimeFormatter.ISO_LOCAL_DATE_TIME), "attemted login", LogStatus.Warning);
-
-		kieSession.insert(logError1);
+		Log logError1 = new Log (LogType.Login, new Machine ("192.168.20.60", false, OperatingSystem.Windows), us, LocalDateTime.parse("2019-04-23T11:11:00", DateTimeFormatter.ISO_LOCAL_DATE_TIME), "login attempt", LogStatus.Warning);
+		
+		//15+ login errors from the same ip address in the last 5 days
+		Machine machine = new Machine("192.168.20.60", false, OperatingSystem.Windows);
+		int dat = -1;
+		for (int i = 0;i<=16;i++) {
+			if (dat<9) {
+				dat++;
+			}
+			Log logError = new Log(LogType.Login, machine, new User("username"+i, Risk.Low, LocalDateTime.now()), LocalDateTime.parse("2019-06-14T11:1"+dat+":00", DateTimeFormatter.ISO_LOCAL_DATE_TIME), "failed login", LogStatus.Error);
+			kieSession.insert(logError);
+		}
+		*/
+		
+		Log logError = new Log();
+		//kieSession.insert(machine);
+		//kieSession.insert(logError1);
+		//kieSession.insert(logError2);
 		kieSession.fireAllRules();
 		kieSession.dispose();
-		return logError1;
+		return logError;
 	}
 }
